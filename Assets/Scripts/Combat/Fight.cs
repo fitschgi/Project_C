38eb7b75f1c5cd3fd1657be777ff5b2c631c92e3
Pepponita <kevin@tulipdesign.ch>
2022-06-08 20:50:55 +0200

    sex

--- Assets/Scripts/Combat/Fight.cs
+++ Assets/Scripts/Combat/Fight.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Fight : MonoBehaviour
 {
     //variables
     public int playerHealth;
@@ -11,18 +13,45 @@ public class NewBehaviourScript : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
-
+    public Button attackButton;
 
     // Start is called before the first frame update
     void Start()
     {
+        /* 
         playerHealth = 100;
         enemyHealth = 100;
+        playerTurn = true; 
+        */
+        attackButton.onClick.AddListener(playerAttack);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!playerTurn)
+        {
+            playerHealth -= 3;
+            playerTurn = true;
+            Debug.Log(playerHealth);
+            Debug.Log(enemyHealth);
+        }
+        if (playerHealth <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+        if (enemyHealth <= 0)
+        {
+            Debug.Log("You Win");
+        }
+    }
+
+    void playerAttack()
+    {
+        if (playerTurn)
+        {
+            enemyHealth -= 10;
+            playerTurn = false;
+        }
     }
 }
