bedaf94c969619df7cb8865c4929d1b5e44db423
fitschgi <jonas.filliger@bluewin.ch>
2022-06-16 02:47:10 +0200

    enjoy
    -fucked Buttons (intentionally bad)
    -gonna fix it using: making presetButton instead of presetText and just taking the entire fuckiing thing and just changing the name

--- Assets/Scripts/Combat/Fight.cs
+++ Assets/Scripts/Combat/Fight.cs
@@ -10,10 +10,13 @@ public class Fight : MonoBehaviour
     public int playerHealth;
     public int enemyHealth;
     public bool playerTurn;
-    public GameObject canvas;
 
+    public GameObject canvas;
     public GameObject player;
     public GameObject enemy;
+    public GameObject preTextset;
+
+    public Sprite buttonbg;
     //public Button attackButton;
     public List<Ability> attackList = new List<Ability>();
 
@@ -21,7 +24,7 @@ public class Fight : MonoBehaviour
     {
         public int damage;
         public string text;
-        public Ability(int damage, string text,)
+        public Ability(int damage, string text)
         {
             this.damage = damage;
             this.text = text;
@@ -37,13 +40,34 @@ public class Fight : MonoBehaviour
         attackList.Add(Punch);
         attackList.Add(Scratch);
         attackList.Add(Lick);
+        int i = 0;
         foreach (Ability ability in attackList)
         {
+            Text presetText = (Text) preTextset.GetComponent(typeof(Text));
             GameObject buttonObject = new GameObject(ability.text);
             RectTransform trans = buttonObject.AddComponent<RectTransform>();
-            buttonObject.transform.SetParent(canvas.transform);
             Button button = buttonObject.AddComponent<Button>();
-            button.onClick.AddListener(playerAttack);
+            Image image = buttonObject.AddComponent<Image>();
+            
+            GameObject text = new GameObject("text");
+            Text tex = text.AddComponent<Text>();
+
+
+            text.transform.SetParent(buttonObject.transform);
+            tex.text = ability.text;
+            tex.alignment = presetText.alignment;
+            tex.font = presetText.font;
+            tex.fontSize = presetText.fontSize;
+
+            buttonObject.transform.SetParent(canvas.transform);
+
+            trans.position = new Vector3(3 * i, 0, 0);
+            trans.localScale = new Vector3(1f,2f,1f);
+
+            image.sprite = buttonbg;
+
+            button.onClick.AddListener(delegate{playerAttack(ability.damage);});
+            i++;
         }
         //attackButton.onClick.AddListener(playerAttack);
     }
@@ -69,11 +93,11 @@ public class Fight : MonoBehaviour
         }
     }
 
-    void playerAttack()
+    void playerAttack(int damage)
     {
         if (playerTurn)
         {
-            enemyHealth -= 10;
+            enemyHealth -= damage;
             playerTurn = false;
         }
     }
